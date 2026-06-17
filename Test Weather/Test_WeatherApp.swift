e8650b00bfaab1b1ad29904be9dcb16ad8e78684
john silver <silnews@gmail.com>
2026-06-17 22:57:47 +0300

    Initial Commit

---
+++ Test Weather/Test_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_WeatherApp.swift
+//  Test Weather
+//
+//  Created by Pavel Puchkov on 17.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Test_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
